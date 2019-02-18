56f695c62565c6a0e396339ff66eb8e5801ff2b4
Margely <47565765+Margely@users.noreply.github.com>
2019-02-18 14:33:11 +0100

    Update ThreeStageDLM.py

--- ThreeStageDLM.py
+++ ThreeStageDLM.py
@@ -15,6 +15,13 @@ import glob
 import PIL.Image
 import copy
 
+pathThick=r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\Data\training\images\*.tif'
+pathThickSaveModel=r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\SaveModel\trainednetwork.h5'
+pathThin=r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\Data\training\images\*.tif'
+pathThinSaveModel=r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\SaveModel\trainednetwork.h5'
+pathFusion=r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\Data\training\images\*.tif'
+pathFusionSaveModel=r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\SaveModel\trainednetwork.h5'
+
 def buildThickcnn():
     
     cnn = keras.models.Sequential()
@@ -115,6 +122,7 @@ def buildThincnn():
     cnn.add(layer11)
     print(layer11.output_shape)
     
+    #Merge layers to create new layers -> green lines
     layer12 = merge([layer4, layer11], mode='sum')
     cnn.add(layer12)
     print(layer12.output_shape)
@@ -241,9 +249,13 @@ def loadImages(impaths,maskpaths,segpaths):
 
     
 def main():
-    
+    #########################################################################################################
+    #########################################################################################################
+    #########################################################################################################
+    #Train ThickSegmenter
     #inputs
-    impaths_all = glob.glob(r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\Data\training\images\*.tif') 
+    #Change path to ground truths with only thick vessels!
+    impaths_all = glob.glob(pathThick) 
     trainingsetsize = 15
     patchsize = 32
     minibatchsize = 200
@@ -297,7 +309,7 @@ def main():
     trainnetwork = True
     
     #initialise the network
-    cnn = buildLeNet()
+    cnn = buildThickcnn()
    
     #and start training
     if trainnetwork: 
@@ -324,18 +336,279 @@ def main():
         plt.figure()
         plt.plot(losslist)    
         
-        cnn.save(r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\SaveModel\trainednetwork.h5')
+        cnn.save(pathThickSaveModel)
     
     else:
-        cnn = keras.models.load_model(r'C:\Users\marge\Documents\Universiteit\Master\Jaar 1\Kwartiel 3\Capita Selecta in Medical Image Analysis\Project\SaveModel\trainednetwork.h5')
+        cnn = keras.models.load_model(pathThickSaveModel)
     
-    #Another training
+    #validate the trained network on the 5 images that were left out during training (numbers 15 to 19)       
+    valimpaths = impaths_all[trainingsetsize:]
+    valmaskpaths = maskpaths_all[trainingsetsize:]
     
-    vis = np.concatenate((img1,img2),axis=0) #change images names!!
+    for j in range(len(valimpaths)):   
+        print(valimpaths[j])
+        
+        valimage = np.array(PIL.Image.open(valimpaths[j]),dtype=np.int16)[:,:,1]
+        valmask = np.array(PIL.Image.open(valmaskpaths[j]),dtype=np.int16)
+        
+        valimage = np.pad(valimage,((halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+        valmask = np.pad(valmask,((halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+        
+        valsamples = np.nonzero(valmask)
+        
+        probimage = np.zeros(valimage.shape)
+        
+        probabilities = np.empty((0,))
+        
+        minibatchsize = 10000 #can be as large as memory allows during testing
+        
+        for i in range(0,len(valsamples[0]),minibatchsize):
+            print('{}/{} samples labelled'.format(i,len(valsamples[0])))
+            
+            if i+minibatchsize < len(valsamples[0]):
+                valbatch = np.arange(i,i+minibatchsize)        
+            else:
+                valbatch = np.arange(i,len(valsamples[0]))        
+            
+            Xval = make2Dpatchestest(valsamples,valbatch,valimage,patchsize)
+                    
+            prob = cnn.predict(Xval, batch_size=minibatchsize)
+            probabilities = np.concatenate((probabilities,prob[:,1]))     
+          
     
-    #Another training
+        for i in range(len(valsamples[0])):
+            probimage[valsamples[0][i],valsamples[1][i]] = probabilities[i]    
+            
+        plt.figure()
+        plt.imshow(probimage,cmap='Greys_r')
+        plt.axis('off')
+      
+    #########################################################################################################
+    #########################################################################################################
+    #########################################################################################################
+    #Train ThinSegmenter
+    #inputs
+    #Change path to ground truths with only thick vessels!
+    impaths_all = glob.glob(pathThick) 
+    trainingsetsize = 15
+    patchsize = 32
+    minibatchsize = 200
+    minibatches = 5000
     
-    vis = np.concatenate((img1,img2),axis=0) #change images names!!
+        
+    #shuffle the images to take a random subset for training later
+    random.shuffle(impaths_all)     
+ 
+    maskpaths_all = copy.deepcopy(impaths_all)
+    segpaths_all = copy.deepcopy(impaths_all)
+    
+    #select the corresponding masks and segmentations  
+    for i in range(len(impaths_all)):
+        maskpaths_all[i] = impaths_all[i].replace('images','mask')
+        maskpaths_all[i] = maskpaths_all[i].replace('.tif','_mask.gif')
+        
+        segpaths_all[i] = impaths_all[i].replace('images','1st_manual')
+        segpaths_all[i] = segpaths_all[i].replace('training.tif','manual1.gif')
+    
+    print(impaths_all)
+    print(maskpaths_all)
+    print(segpaths_all)    
+    
+    #select the first 15 images as training set, the other 5 will be used for validation
+    impaths = impaths_all[:trainingsetsize]
+    maskpaths = maskpaths_all[:trainingsetsize]
+    segpaths = segpaths_all[:trainingsetsize]  
+    
+    #load the training images    
+    images, masks, segmentations = loadImages(impaths,maskpaths,segpaths)   
+    
+    print(images.shape)
+    print(masks.shape)
+    print(segmentations.shape)        
+    
+    #pad the images with zeros to allow patch extraction at all locations
+
+    halfsize = int(patchsize/2)    
+    images = np.pad(images,((0,0),(halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+    masks = np.pad(masks,((0,0),(halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+    segmentations = np.pad(segmentations,((0,0),(halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+    
+    #separately select the positive samples (vessel) and negative samples (background)
+    positivesamples = np.nonzero(segmentations)
+    negativesamples = np.nonzero(masks-segmentations)   
+   
+    print(len(positivesamples[0]))
+    print(len(negativesamples[0]))  
+
+    trainnetwork = True
+    
+    #initialise the network
+    cnn = buildThickcnn()
+   
+    #and start training
+    if trainnetwork: 
+        losslist = []
+        
+        for i in range(minibatches):
+            
+            posbatch = random.sample(list(range(len(positivesamples[0]))),int(minibatchsize/2))
+            negbatch = random.sample(list(range(len(negativesamples[0]))),int(minibatchsize/2))
+             
+            Xpos, Ypos = make2Dpatches(positivesamples,posbatch,images,32,1)
+            Xneg, Yneg = make2Dpatches(negativesamples,negbatch,images,32,0)
+          
+            Xtrain = np.vstack((Xpos,Xneg))
+            Ytrain = np.vstack((Ypos,Yneg))            
+           
+            loss = cnn.train_on_batch(Xtrain,Ytrain)
+            losslist.append(loss)
+            print('Batch: {}'.format(i))
+            print('Loss: {}'.format(loss))
+                
+        
+        plt.close('all')
+        plt.figure()
+        plt.plot(losslist)    
+        
+        cnn.save(pathThinSaveModel)
+    
+    else:
+        cnn = keras.models.load_model(pathThinSaveModel)
+    
+    #validate the trained network on the 5 images that were left out during training (numbers 15 to 19)       
+    valimpaths = impaths_all[trainingsetsize:]
+    valmaskpaths = maskpaths_all[trainingsetsize:]
+    
+    for j in range(len(valimpaths)):   
+        print(valimpaths[j])
+        
+        valimage = np.array(PIL.Image.open(valimpaths[j]),dtype=np.int16)[:,:,1]
+        valmask = np.array(PIL.Image.open(valmaskpaths[j]),dtype=np.int16)
+        
+        valimage = np.pad(valimage,((halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+        valmask = np.pad(valmask,((halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+        
+        valsamples = np.nonzero(valmask)
+        
+        probimage = np.zeros(valimage.shape)
+        
+        probabilities = np.empty((0,))
+        
+        minibatchsize = 10000 #can be as large as memory allows during testing
+        
+        for i in range(0,len(valsamples[0]),minibatchsize):
+            print('{}/{} samples labelled'.format(i,len(valsamples[0])))
+            
+            if i+minibatchsize < len(valsamples[0]):
+                valbatch = np.arange(i,i+minibatchsize)        
+            else:
+                valbatch = np.arange(i,len(valsamples[0]))        
+            
+            Xval = make2Dpatchestest(valsamples,valbatch,valimage,patchsize)
+                    
+            prob = cnn.predict(Xval, batch_size=minibatchsize)
+            probabilities = np.concatenate((probabilities,prob[:,1]))     
+          
+    
+        for i in range(len(valsamples[0])):
+            probimage[valsamples[0][i],valsamples[1][i]] = probabilities[i]    
+            
+        plt.figure()
+        plt.imshow(probimage,cmap='Greys_r')
+        plt.axis('off')
+        
+        
+    #########################################################################################################
+    #########################################################################################################
+    #########################################################################################################
+    #Train FusionSegmenter
+    #inputs
+    #Change path to ground truths with only thick vessels!
+    impaths_all = glob.glob(pathThick) 
+    trainingsetsize = 15
+    patchsize = 32
+    minibatchsize = 200
+    minibatches = 5000
+    
+        
+    #shuffle the images to take a random subset for training later
+    random.shuffle(impaths_all)     
+ 
+    maskpaths_all = copy.deepcopy(impaths_all)
+    segpaths_all = copy.deepcopy(impaths_all)
+    
+    #select the corresponding masks and segmentations  
+    for i in range(len(impaths_all)):
+        maskpaths_all[i] = impaths_all[i].replace('images','mask')
+        maskpaths_all[i] = maskpaths_all[i].replace('.tif','_mask.gif')
+        
+        segpaths_all[i] = impaths_all[i].replace('images','1st_manual')
+        segpaths_all[i] = segpaths_all[i].replace('training.tif','manual1.gif')
+    
+    print(impaths_all)
+    print(maskpaths_all)
+    print(segpaths_all)    
+    
+    #select the first 15 images as training set, the other 5 will be used for validation
+    impaths = impaths_all[:trainingsetsize]
+    maskpaths = maskpaths_all[:trainingsetsize]
+    segpaths = segpaths_all[:trainingsetsize]  
+    
+    #load the training images    
+    images, masks, segmentations = loadImages(impaths,maskpaths,segpaths)   
+    
+    print(images.shape)
+    print(masks.shape)
+    print(segmentations.shape)        
+    
+    #pad the images with zeros to allow patch extraction at all locations
+
+    halfsize = int(patchsize/2)    
+    images = np.pad(images,((0,0),(halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+    masks = np.pad(masks,((0,0),(halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+    segmentations = np.pad(segmentations,((0,0),(halfsize,halfsize),(halfsize,halfsize)),'constant', constant_values=0)
+    
+    #separately select the positive samples (vessel) and negative samples (background)
+    positivesamples = np.nonzero(segmentations)
+    negativesamples = np.nonzero(masks-segmentations)   
+   
+    print(len(positivesamples[0]))
+    print(len(negativesamples[0]))  
+
+    trainnetwork = True
+    
+    #initialise the network
+    cnn = buildThickcnn()
+   
+    #and start training
+    if trainnetwork: 
+        losslist = []
+        
+        for i in range(minibatches):
+            
+            posbatch = random.sample(list(range(len(positivesamples[0]))),int(minibatchsize/2))
+            negbatch = random.sample(list(range(len(negativesamples[0]))),int(minibatchsize/2))
+             
+            Xpos, Ypos = make2Dpatches(positivesamples,posbatch,images,32,1)
+            Xneg, Yneg = make2Dpatches(negativesamples,negbatch,images,32,0)
+          
+            Xtrain = np.vstack((Xpos,Xneg))
+            Ytrain = np.vstack((Ypos,Yneg))            
+           
+            loss = cnn.train_on_batch(Xtrain,Ytrain)
+            losslist.append(loss)
+            print('Batch: {}'.format(i))
+            print('Loss: {}'.format(loss))
+                
+        
+        plt.close('all')
+        plt.figure()
+        plt.plot(losslist)    
+        
+        cnn.save(pathFusionSaveModel)
+    
+    else:
+        cnn = keras.models.load_model(pathFusionSaveModel)
     
     #validate the trained network on the 5 images that were left out during training (numbers 15 to 19)       
     valimpaths = impaths_all[trainingsetsize:]
@@ -380,7 +653,8 @@ def main():
         plt.axis('off')
 
     return   
-     
+  
+    #vis = np.concatenate((img1,img2),axis=0) #change images names!!
     
 if __name__=="__main__":
     main()
