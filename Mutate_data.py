71790da47e2c74c3f6123590ffd0a39414363a48
Mathijs de Boer <czorio4@gmail.com>
2019-02-22 15:36:00 +0100

    Added Gradient mutation Disabled rotation due to bugginess

--- Mutate_data.py
+++ Mutate_data.py
@@ -9,7 +9,7 @@ import SimpleITK as sitk
 from os import listdir
 from os.path import isfile, join
 
-def mutate_data(images, masks, segmentations, noise_strength=20, new_records=1000, seed=314159):
+def mutate_data(images, masks, segmentations, noise_strength=20, new_records=1000, seed=314159, gradient_min=0.7, gradient_max=1.0):
     count = 0
     random.seed(seed)
 
@@ -22,6 +22,8 @@ def mutate_data(images, masks, segmentations, noise_strength=20, new_records=100
     print("\tNoise Strength = {}".format(noise_strength))
     print("\tNew Records = {}".format(new_records))
     print("\tSeed = {}".format(seed))
+    print("\tGradient Min = {}".format(gradient_min))
+    print("\tGradient Max = {}".format(gradient_max))
     while count < new_records:
         # Fetch random patient images
         print("{}/{}".format(count, new_records), end='\r')
@@ -34,50 +36,34 @@ def mutate_data(images, masks, segmentations, noise_strength=20, new_records=100
 
         # Mutate images
         # Determine which operation to perform
-        operation = random.randint(0, 1)
+        operation = random.randint(0, 2)
 
         # Random Rotation
-        if operation == 0:
-
+        if operation == 3:
             # Determine new orientation
-            rot = random.randint(0, 2)
-
-            # There's probably a cleaner way to do this, but it works
-            if rot == 0:
-                # Rotate once
-                np.rot90(image)
-
-                np.rot90(mask)
-
-                np.rot90(segmentation)
-            elif rot == 1:
-                # Rotate twice
-                np.rot90(image)
-                np.rot90(image)
-
-                np.rot90(mask)
-                np.rot90(mask)
-
-                np.rot90(segmentation)
-                np.rot90(segmentation)
-            else:
-                # Rotate thrice
-                np.rot90(image)
-                np.rot90(image)
-                np.rot90(image)
-
-                np.rot90(mask)
-                np.rot90(mask)
-                np.rot90(mask)
-
-                np.rot90(segmentation)
-                np.rot90(segmentation)
-                np.rot90(segmentation)
+            rot = random.randint(1, 3)
+            image = np.rot90(image, k=rot)
+            mask = np.rot90(mask, k=rot)
+            segmentation = np.rot90(segmentation, k=rot)
 
         # Random noise (normal distribution)
         elif operation == 1:
             # Noise is only applied to the actual image
             image = np.random.normal(image, noise_strength)
+
+        # Add gradient
+        elif operation == 2:
+            # Generate gradient and give it a random orientation
+            gradient = np.zeros(image.shape)
+            length = image.shape[0]
+            for i in range(length):
+                gradient[i] = gradient_min + (gradient_max - gradient_min) * (i / length)
+            rot = random.randint(0, 1)
+            if rot == 1:
+                gradient = np.rot90(gradient, k=2)
+
+            # Apply gradient
+            image = image * gradient
 
         # Save
         res_im = np.append(res_im, [image], axis=0)
