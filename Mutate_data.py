1236734c337903c59eb3a2c0bc98224826d47d63
Mathijs de Boer <czorio4@gmail.com>
2019-02-25 11:59:33 +0100

    Added more mutations Replaced Rotate with Flip

--- Mutate_data.py
+++ Mutate_data.py
@@ -4,12 +4,14 @@ import glob
 import numpy as np
 import os
 import random
+import scipy
 import SimpleITK as sitk
 
 from os import listdir
 from os.path import isfile, join
+from scipy import ndimage
 
-def mutate_data(images, masks, segmentations, noise_strength=20, new_records=1000, seed=314159, gradient_min=0.7, gradient_max=1.0):
+def mutate_data(images, masks, segmentations, noise_strength=64, new_records=1000, seed=314159):
     count = 0
     random.seed(seed)
 
@@ -22,8 +24,6 @@ def mutate_data(images, masks, segmentations, noise_strength=20, new_records=100
     print("\tNoise Strength = {}".format(noise_strength))
     print("\tNew Records = {}".format(new_records))
     print("\tSeed = {}".format(seed))
-    print("\tGradient Min = {}".format(gradient_min))
-    print("\tGradient Max = {}".format(gradient_max))
     while count < new_records:
         # Fetch random patient images
         print("{}/{}".format(count, new_records), end='\r')
@@ -36,34 +36,43 @@ def mutate_data(images, masks, segmentations, noise_strength=20, new_records=100
 
         # Mutate images
         # Determine which operation to perform
-        operation = random.randint(0, 2)
+        operation = random.randint(0, 3)
 
         # Random Rotation
-        if operation == 3:
+        if operation == 0:
             # Determine new orientation
-            rot = random.randint(1, 3)
-            image = np.rot90(image, k=rot)
-            mask = np.rot90(mask, k=rot)
-            segmentation = np.rot90(segmentation, k=rot)
+            rot = random.randint(0, 2)
+            
+            if rot == 0:
+                # Flip over one axis
+                image = np.flipud(image)
+                mask = np.flipud(mask)
+                segmentation = np.flipud(segmentation)
+            elif rot ==1:
+                # Flip over one axis
+                image = np.fliplr(image)
+                mask = np.fliplr(mask)
+                segmentation = np.fliplr(segmentation)
+            else:
+                # Flip over both axes
+                image = np.flipud(image)
+                mask = np.flipud(mask)
+                segmentation = np.flipud(segmentation)
+
+                image = np.fliplr(image)
+                mask = np.fliplr(mask)
+                segmentation = np.fliplr(segmentation)
 
         # Random noise (normal distribution)
         elif operation == 1:
             # Noise is only applied to the actual image
             image = np.random.normal(image, noise_strength)
-
-        # Add gradient
+        # Contrast change
         elif operation == 2:
-            # Generate gradient and give it a random orientation
-            gradient = np.zeros(image.shape)
-            length = image.shape[0]
-            for i in range(length):
-                gradient[i] = gradient_min + (gradient_max - gradient_min) * (i / length)
-            rot = random.randint(0, 1)
-            if rot == 1:
-                gradient = np.rot90(gradient, k=2)
-
-            # Apply gradient
-            image = image * gradient
+            image = (float(random.randint(50, 150)) / 100.0) * image
+        # Blur Kernel
+        elif operation == 3:
+            image = ndimage.gaussian_filter(image, 2)
 
         # Save
         res_im = np.append(res_im, [image], axis=0)
