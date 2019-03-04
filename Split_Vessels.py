cffdb70f895f23ec70b528ce32312a0a0f77377a
Mathijs de Boer <czorio4@gmail.com>
2019-03-04 20:25:53 +0100

    Made Split_Vessels work for all segmentations in a directory

--- Split_Vessels.py
+++ Split_Vessels.py
@@ -1,40 +1,47 @@
+import glob
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 from PIL import Image
 from skimage.draw import circle
 from skimage.morphology import medial_axis
 
-thickness_threshold = 3
+thickness_threshold = 2.5
+path = r"D:\UU\CSTUe\training\1st_manual"
 
-# Load
-img = Image.open(r"D:\UU\CSTUe\training\1st_manual\21_manual1.gif")
-arr = np.array(img)
+seg_paths = glob.glob(os.path.join(path, "*.gif"))
+for seg_path in seg_paths:
+    print(seg_path[-14:], end="\r")
+    
+    # Load
+    img = Image.open(seg_path)
+    arr = np.array(img)
 
-# Skeletonize and find distances through the Medial Axis Transform
-skeleton, distance = medial_axis(arr, return_distance=True)
-med_axis = skeleton * distance
+    # Skeletonize and find distances through the Medial Axis Transform
+    skeleton, distance = medial_axis(arr, return_distance=True)
+    med_axis = skeleton * distance
 
-# Split in thin and thick
-thin_mat = np.array(med_axis)
-thin_mat[thin_mat > thickness_threshold] = 0
-thick_mat = np.array(med_axis)
-thick_mat[thick_mat <= thickness_threshold] = 0
+    # Split in thin and thick
+    thin_mat = np.array(med_axis)
+    thin_mat[thin_mat > thickness_threshold] = 0
+    thick_mat = np.array(med_axis)
+    thick_mat[thick_mat <= thickness_threshold] = 0
 
-thin_vessels = np.zeros(arr.shape)
-thick_vessels = np.zeros(arr.shape)
+    thin_vessels = np.zeros(arr.shape)
+    thick_vessels = np.zeros(arr.shape)
 
-# Draw Circles
-for coord in np.argwhere(thin_mat > 0):
-    rr, cc = circle(coord[0], coord[1], thin_mat[coord[0], coord[1]], shape=thin_vessels.shape)
-    thin_vessels[rr, cc] = 1
+    # Draw Circles
+    for coord in np.argwhere(thin_mat > 0):
+        rr, cc = circle(coord[0], coord[1], thin_mat[coord[0], coord[1]], shape=thin_vessels.shape)
+        thin_vessels[rr, cc] = 1
 
-for coord in np.argwhere(thick_mat > 0):
-    rr, cc = circle(coord[0], coord[1], thick_mat[coord[0], coord[1]], shape=thin_vessels.shape)
-    thick_vessels[rr, cc] = 1
+    for coord in np.argwhere(thick_mat > 0):
+        rr, cc = circle(coord[0], coord[1], thick_mat[coord[0], coord[1]], shape=thin_vessels.shape)
+        thick_vessels[rr, cc] = 1
 
-# Write
-thin_image = Image.fromarray((thin_vessels * 255).astype(np.uint8))
-thin_image.save(r"D:\UU\CSTUe\training\1st_manual\thin\21_manual1.gif")
-thick_image = Image.fromarray((thick_vessels * 255).astype(np.uint8))
-thick_image.save(r"D:\UU\CSTUe\training\1st_manual\thick\21_manual1.gif")
+    # Write
+    thin_image = Image.fromarray((thin_vessels * 255).astype(np.uint8))
+    thin_image.save(os.path.join(path, "thin", seg_path[-14:]))
+    thick_image = Image.fromarray((thick_vessels * 255).astype(np.uint8))
+    thick_image.save(os.path.join(path, "thick", seg_path[-14:]))
